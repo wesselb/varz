5cc51c1806e4cd80784f04d45d3e9bb730639b6d
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2019-11-06 20:59:21 +0000

    Test _extract_prefix_and_f

--- tests/test_spec.py
+++ tests/test_spec.py
@@ -1,1 +1,10 @@
+from varz.spec import _extract_prefix_and_f
 
+
+def test_extract_prefix_and_f():
+    def f():
+        pass
+
+    assert _extract_prefix_and_f(None) == ('', None)
+    assert _extract_prefix_and_f(f) == ('', f)
+    assert _extract_prefix_and_f('test') == ('test', None)
