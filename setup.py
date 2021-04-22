41732a37a9e6131c1e0a1fa9654410041642775f
Wessel Bruinsma <wessel.p.bruinsma@gmail.com>
2021-04-22 09:32:56 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,12 @@
 from setuptools import find_packages, setup
 
-requirements = ["numpy>=1.16", "scipy>=1.3", "plum-dispatch", "backends>=0.5.0", "wbml"]
+requirements = [
+    "numpy>=1.16",
+    "scipy>=1.3",
+    "plum-dispatch>=1",
+    "backends>=1",
+    "wbml>=0.3",
+]
 
 setup(
     packages=find_packages(exclude=["docs"]),
